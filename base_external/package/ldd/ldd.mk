7cff9e93a4586167ebd672581e13cc2faa7252eb
lizsPC <lizzyhllrn@gmail.com>
2023-11-06 13:36:47 -0700

    updated makefile

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -10,11 +10,11 @@ LDD_MODULE_SUBDIRS = scull
 LDD_MODULE_SUBDIRS = misc-modules
 
 define LDD_INSTALL_TARGET_CMDS
-	mkdir -p $(BR2_ROOTFS_OVERLAY)/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(BR2_ROOTFS_OVERLAY)/bin/scull_load
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(BR2_ROOTFS_OVERLAY)/bin/scull_unload	
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(BR2_ROOTFS_OVERLAY)/bin/module_load
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(BR2_ROOTFS_OVERLAY)/bin/module_unload
+	
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin	
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	
 endef
 
