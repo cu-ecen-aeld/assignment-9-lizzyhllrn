39d918d4a4a4fa1a1701356834c094a2c15b145a
lizsPC <lizzyhllrn@gmail.com>
2023-11-12 18:55:05 -0700

    added AESD_CHAR switch

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
-set -e
 
-NAME=lddmodules
 case "$1" in
     start)
         echo "loading mods"
         scull_load
         module_load faulty
         modprobe hello
-    ;;
+        ;;
     stop)
         eacho "unloading mods"
         scull_unload
         module_unload faulty
         rmmod hello
-
-    ;;
+        ;;
     *)
         echo "Usage: $0 {start|stop}"
         exit 1
