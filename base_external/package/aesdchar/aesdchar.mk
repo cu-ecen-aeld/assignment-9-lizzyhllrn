1dd576d0f3211601ff375e1eefcaec787a39a9ac
lizsPC <lizzyhllrn@gmail.com>
2023-11-12 22:00:34 -0700

    added to init script

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,4 +1,4 @@
-AESDCHAR_VERSION = f0599b0dbbfe4b54b32da597f452e4d3a7baff62
+AESDCHAR_VERSION = 3161c7924beefa5be2f61ca5c7042a8e41008c60
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -12,6 +12,7 @@ define AESDCHAR_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/bin/
 	
 endef
 
